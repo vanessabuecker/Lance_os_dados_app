5bd720e853c05c05631513b303c79a7389b5797d
Vanessa Buecker <v.buecker@rocketmail.com>
2021-06-12 13:42:31 -0300

    "update"

--- app/src/main/java/com/vbuecker/app_dice_play_devventure/RegisterUserFragment.kt
+++ app/src/main/java/com/vbuecker/app_dice_play_devventure/RegisterUserFragment.kt
@@ -1,28 +1,68 @@
 package com.vbuecker.app_dice_play_devventure
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.vbuecker.app_lanamento_dados_devventure.R
-import com.vbuecker.app_lanamento_dados_devventure.databinding.FragmentRegisterUserBinding
 
 
 class RegisterUserFragment : Fragment() {
 
-    private var binding :FragmentRegisterUserBinding? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    /*var binding: FragmentRegisterUserBinding? = null
+    @SuppressLint("EditTextCommits")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentRegisterUserBinding.inflate(inflater, container, false)
+
+        val usernameCached = getUsername()
+        usernameCached?.let { goUserActivity(usernameCached) }
+
+        val edit_text_input_name = binding?.editTextInputName
+
+        binding?.buttonSpecial?.setOnClickListener {
+            val name = binding?.editTextInputName?.text
+            val nameString = name.toString().trim()
+            if (nameString == "") {
+                Toast.makeText(
+                    context,
+                    R.string.toast_type_your_name,
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                saveUsername(nameString)
+                goUserActivity(nameString)
+            }
+        }
+
+        binding?.buttonSpecial?.setOnClickListener {
+            val player = edit_text_input_name?.text.toString()
+            val intent = Intent(this, SpecialActivity::class.java)
+            intent.putExtra("username", player)
+            startActivity(intent)
+        }
         return binding?.root
     }
 
+    private fun getUsername(): String? {
+        val sharedPref =
+            context?.getSharedPreferences("LoginPreferences", Context.MODE_PRIVATE) ?: return null
+        return sharedPref.getString("username", null)
+    }
+
+    private fun saveUsername(username: String) {
+        val sharedPref =
+            context?.getSharedPreferences("LoginPreferences", Context.MODE_PRIVATE) ?: return
+        with(sharedPref.edit()) {
+            putString("username", username)
+            commit()
+        }
+    }
+
+    private fun goUserActivity(username: String) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("Username", username)
+        startActivity(intent)
+    }*/
+
 }
+
